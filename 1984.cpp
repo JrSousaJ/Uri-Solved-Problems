89f3c5595c19e4e28fbf5ddf0882303f2f9dbbbe
PaiDSanto <35965251+PaiDSanto@users.noreply.github.com>
2018-04-10 19:53:47 -0300

    Create 1984.cpp

---
+++ 1984.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+long long int i ,q=0;
+int j;
+        scanf("%lld",&i);
+        while(i>0.0)
+        {
+             q=i%10;
+        i=i/10;
+        printf("%d",q);
+            }
+
+
+        printf("\n");
+
+
+}
