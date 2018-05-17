2a3199d9edc3d3e97fc5c5f79e180223a6716dbc
PaiDSanto <35965251+PaiDSanto@users.noreply.github.com>
2018-05-17 19:12:42 -0300

    Update 2557.cpp

--- 2557.cpp
+++ 2557.cpp
@@ -9,29 +9,15 @@ int main()
    char comp;
    while(scanf(" %[^+]+%[^=]=%s",s,s2,s3)!=EOF)
    {
-   		 long long int ans=0;
-   		 int x=0,y=0;
-   		if(s[0]=='R')
-        {
-            sscanf(s2,"%d",&x);
-            sscanf(s3,"%d",&y);
-            ans=y-x;
-        }
-        else if(s2[0]=='L')
-        {
-            sscanf(s,"%d",&x);
-            sscanf(s3,"%d",&y);
-             ans=y-x;
-        }
-        else if(s3[0]=='J')
-        {
-            sscanf(s,"%d",&x);
-            sscanf(s2,"%d",&y);
-            ans=x+y;
-        }
+   	long long int ans=0;
+   	int x=0,y=0,z=0;
+   	sscanf(s,"%d",&x);
+        sscanf(s2,"%d",&y);
+        sscanf(s3,"%d",&z);
+   	if(s[0]=='R') ans=z-y;
+        else if(s2[0]=='L') ans=z-x;
+        else if(s3[0]=='J') ans=x+y;
         printf("%lld\n",ans);
-
-
    }
 	return 0;
 }
