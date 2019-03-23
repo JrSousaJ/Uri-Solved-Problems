e1b6a1a1d09906f5083e2dc46bf69ed3e33bf808
JrSousaJ <jairosousajunior@gmail.com>
2019-03-23 00:13:33 -0300

    feat: 2569.cpp

--- 2569.cpp
+++ 2569.cpp
@@ -1,27 +1,45 @@
 #include<bits/stdc++.h>
-#define ll long long
+
 using namespace std;
 
 int main()
 {
-	int n,n3;
-	char s;
-	int ans=0;
-	scanf("%d %c %d",&n,&s,&n3);
-	if(s=='+' && n+n3==7)
-	{
-		printf("0\n");
-	}
-	else if(s=='+')
-		ans=n+n3;
-	else if(s=='x')
-		ans=n*n3;
-	char s1[1000];
-	sprintf(s1,"%d",ans);
-	for(int i=0;i<strlen(s1);i++)
-	{
-		if(s1[i]!='7')
-			printf("%c",s1[i]);
-	}
-	return 0;
+    string s, o, s1;
+    cin >> s >> o >> s1;
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]=='7')s[i]='0';
+    }
+    for(int i=0;i<s1.size();i++)
+    {
+        if(s1[i]=='7')s1[i]='0';
+    }
+    stringstream f;
+    stringstream h;
+    long long int b, a, ans;
+    b=a=ans=0;
+    f << s;
+    f >> a;
+    h << s1;
+    h >> b;
+    if(o=="+")
+        ans=b+a;
+    else
+        ans=b*a;
+    string aux;
+    while(ans>0)
+    {
+        char x=(ans%10)+48;
+        if(x=='7')x='0';
+        aux+=x;
+        ans/=10;
+    }
+    reverse(aux.begin(),aux.end());
+    stringstream bud;
+    long long int val;
+    bud << aux;
+    bud >> val;
+    printf("%lld\n",val);
+
+    return 0;
 }
